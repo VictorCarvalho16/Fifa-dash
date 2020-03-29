a79e2ab51849998ddd8ca459dd434119a635f1eb
VictorCarvalho16 <victor.inf2011@hotmail.com>
2020-03-29 16:00:57 -0300

    funcionalidades de loading e visual de navegação

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,10 +2,19 @@ const playersMain = document.getElementById('players')
 const page1Link = document.getElementById('page-1')
 const page2Link = document.getElementById('page-2')
 const page3Link = document.getElementById('page-3')
+const page4Link = document.getElementById('page-4')
+const page5Link = document.getElementById('page-5')
+const page6Link = document.getElementById('page-6')
 
 async function retrievePlayersInfo(skip, limit) {
     const baseUrl = 'https://fifagama.herokuapp.com/fifa19'
     let response = await fetch(`${baseUrl}/${skip}/${limit}`)
+    playersMain.innerHTML = `
+    <div class="d-flex justify-content-center my-auto">
+        <span>Loading...</span>
+        <div class="spinner-border" role="status"></div>
+    </div>
+    `
 
     return response.json();
 }
@@ -35,7 +44,6 @@ function buildHTML(players) {
         if(count % 3 === 0 || count === 0) {
             lineDiv = document.createElement('div')
             lineDiv.className = 'row align-items-center mt-5'
-            console.log(lineDiv)
         }
 
         lineDiv.innerHTML += html
@@ -45,15 +53,58 @@ function buildHTML(players) {
     })
 }
 
+function changeActivePagination(page) {
+    let pagesItemsButtons = document.getElementsByClassName('page-item')
+    for (let index = 0; index < pagesItemsButtons.length; index++) {
+        pagesItemsButtons[index].classList.remove('active')
+    }
+
+
+    let pageItemActive = document.getElementById(`page-${page}`)
+    console.log(pageItemActive)
+    pageItemActive.classList.add('active')
+}
+
+window.onload = retrievePlayersInfo(0, 9).then(players => buildHTML(players))
+
 page1Link.addEventListener('click', () => {
     retrievePlayersInfo(0, 9)
         .then(players => buildHTML(players))
+    
+    changeActivePagination(1)
 })
+
 page2Link.addEventListener('click', () => {
     retrievePlayersInfo(9, 9)
         .then(players => buildHTML(players))
+
+    changeActivePagination(2)
 })
+
 page3Link.addEventListener('click', () => {
     retrievePlayersInfo(18, 9)
         .then(players => buildHTML(players))
+
+    changeActivePagination(3)
+})
+
+page4Link.addEventListener('click', () => {
+    retrievePlayersInfo(27, 9)
+        .then(players => buildHTML(players))
+
+    changeActivePagination(4)
+})
+
+page5Link.addEventListener('click', () => {
+    retrievePlayersInfo(36, 9)
+        .then(players => buildHTML(players))
+
+    changeActivePagination(5)
+})
+
+page6Link.addEventListener('click', () => {
+    retrievePlayersInfo(45, 9)
+        .then(players => buildHTML(players))
+
+    changeActivePagination(6)
 })
