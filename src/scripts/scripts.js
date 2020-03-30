e6e640a158144f09efffa50997f014781c40cfec
unknown <victor.inf2011@hotmail.com>
2020-03-30 17:56:07 -0300

    Adicionando graficos com CharJs

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -19,13 +19,16 @@ function buildHTML(players) {
     playersMain.innerHTML = ''
     count = 0
     var lineDiv
-    players.map(player => {
-        player = player.data      
 
+    players.map(player => {
+        player = player.data
+
+        playerPassing = parseInt((parseInt(player.ShortPassing) + parseInt(player.LongPassing)) / 2)
         let html = `
         <div class="col-4">
+            <a data-toggle="modal" data-target="#info${player.ID}">
             <div class="card mx-auto card-player" style="width: 16rem;">
-                <img class="card-img-top mx-auto" style="width: 12rem;" src="${player.Photo.replace('/4/', '/10/')}"
+                <img class="card-img-top mx-auto mt-2" style="width: 12rem;" src="${player.Photo.replace('/4/', '/10/')}"
                     alt="${player.Name}'s Photo">
                 <div class="card-body text-center">
                     <p class="mb-2 player-name"><strong>${player.Name}</strong></p>
@@ -35,9 +38,52 @@ function buildHTML(players) {
                     <p class="mb-0 pb-2">Club <img src="${player['Club Logo']}" alt="${player.Club} "></p>
                 </div>
             </div>
+            <a>
         </div>
-        `  
-        if(count % 3 === 0 || count === 0) {
+
+        <!-- Modal -->
+        <div class="modal fade" id="info${player.ID}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">${player.Name}'s Status</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            ${player.Dribbling}<hr/>
+            ${player.Acceleration}<hr/>
+            ${player.BallControl}<hr/>
+            ${player.Finishing}<hr/>
+            ${player.Positioning}<hr/>
+            ${player.Finishing}<hr/>
+            ${player.Strength}<hr/>
+            ${player.Interceptions}<hr/>
+            ${playerPassing}<hr/>
+
+            <div class="content">
+                <div class="wrapper" style="max-width: 512px; margin: auto">
+                    <canvas id="${player.ID}-graphic"></canvas>
+                </div>
+            </div>
+            <hr/>
+            Preferred Foot <strong>${player['Preferred Foot']}</strong>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <script>
+
+        </script
+
+        `
+
+        if (count % 3 === 0 || count === 0) {
             lineDiv = document.createElement('div')
             lineDiv.className = 'row align-items-center mt-5 mx-0'
         }
@@ -50,15 +96,45 @@ function buildHTML(players) {
 
         count++
     })
+
+    players.map(player => {
+        player = player.data
+        new Chart(document.getElementById(`${player.ID}-graphic`),{
+            "type":"radar",
+            "data":{
+                "labels":[
+                    "Dribbling","Drinking","Sleeping","Designing","Coding","Cycling","Running"],
+                "datasets":[{
+                    "label":"My Second Dataset","data":[player.Dribbling,48,40,19,96,27,100],
+                    "fill":true,
+                    "backgroundColor":"rgba(54, 162, 235, 0.2)",
+                    "borderColor":"rgb(54, 162, 235)",
+                    "pointBackgroundColor":"rgb(54, 162, 235)",
+                    "pointBorderColor":"#fff",
+                    "pointHoverBackgroundColor":"#fff",
+                    "pointHoverBorderColor":"rgb(54, 162, 235)"}
+                ]},
+                "options":{
+                    "elements":{
+                        "line":{
+                            "tension":0,
+                            "borderWidth":3
+                        }
+                    }
+                }
+        });
+    })
 }
 
 function Loading() {
     navPages.style.display = 'none'
     htmlPage.style.height = '100%'
-    playersMain.innerHTML = `<div class="d-flex justify-content-center my-auto loading">
-    <span>Loading...</span>
-    <div class="spinner-border" role="status"></div>
-    </div>`
+    playersMain.innerHTML = `
+    <div class="d-flex justify-content-center my-auto loading">
+        <span>Loading...</span>
+        <div class="spinner-border" role="status"></div>
+    </div>
+    `
 }
 
 function changeActivePagination(page) {
@@ -68,53 +144,51 @@ function changeActivePagination(page) {
         pagesItemsButtons[index].classList.remove('active')
     }
 
-
     let pageItemActive = document.getElementById(`page-${page}`)
-    console.log(pageItemActive)
     pageItemActive.classList.add('active')
 }
 
-window.onload = Loading() ,retrievePlayersInfo(0, 9).then(players => buildHTML(players))
+window.onload = Loading(), retrievePlayersInfo(0, 9).then(players => buildHTML(players))
 
 page1Link.addEventListener('click', () => {
     retrievePlayersInfo(0, 9)
 
-    changeActivePagination(1)    
-    .then(players => buildHTML(players))
+    changeActivePagination(1)
+        .then(players => buildHTML(players))
 
 })
 
 page2Link.addEventListener('click', () => {
     changeActivePagination(2)
-    
+
     retrievePlayersInfo(9, 9)
         .then(players => buildHTML(players))
 })
 
 page3Link.addEventListener('click', () => {
     changeActivePagination(3)
-    
+
     retrievePlayersInfo(18, 9)
         .then(players => buildHTML(players))
 })
 
 page4Link.addEventListener('click', () => {
     changeActivePagination(4)
-    
+
     retrievePlayersInfo(27, 9)
         .then(players => buildHTML(players))
 })
 
 page5Link.addEventListener('click', () => {
     changeActivePagination(5)
-    
+
     retrievePlayersInfo(36, 9)
         .then(players => buildHTML(players))
 })
 
 page6Link.addEventListener('click', () => {
     changeActivePagination(6)
-    
+
     retrievePlayersInfo(45, 9)
         .then(players => buildHTML(players))
 })
